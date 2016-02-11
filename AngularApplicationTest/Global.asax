3c7080a0a057751d26e26d6bc4b282b6abfe638e
William Danielsson <william.danielsson@valtech.com>
2016-02-11 15:13:06 +0100

    Added template with some working Angular code

---
+++ AngularApplicationTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularApplicationTest.MvcApplication" Language="C#" %>
---
+++ AngularApplicationTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularApplicationTest.Startup))]
+namespace AngularApplicationTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
